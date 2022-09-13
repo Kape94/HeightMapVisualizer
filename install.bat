ebc8e9e6abdd62a53d2697fb946b0fdc779d4f30
Kleber <kleberkapelinski@gmail.com>
2022-09-13 12:22:05 -0300

    Fixing compatibility problem.

--- install.bat
+++ install.bat
@@ -4,6 +4,12 @@ cd vcpkg
 
 call bootstrap-vcpkg.bat
 
+vcpkg install freeglut
 vcpkg --triplet x64-windows install freeglut
+
+vcpkg install glew
 vcpkg --triplet x64-windows install glew
+
+
+vcpkg install glm
 vcpkg --triplet x64-windows install glm
